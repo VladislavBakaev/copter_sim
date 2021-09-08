a1f3827b63a8f7f4cf3553bec071516b38b0808e
vladislavbakaev <bakaev.98@bk.ru>
2021-09-08 15:46:40 +0300

    fix filter

--- copter_controller/scripts/kalmanFilter.py
+++ copter_controller/scripts/kalmanFilter.py
@@ -1,42 +1,9 @@
+#coding=utf-8
+
 import numpy as np
 from math import sin, cos, tan
 import time 
 import matplotlib.pyplot as plt
-
-class KalmanFilter():  
-    def __init__(self, n):
-        self.Q = np.array([0.005]*n)
-        self.R = np.array([0.1]*n)
-        self.P_k_k1 = np.array([1]*n)
-        self.Kg = np.array([0]*n)
-        self.P_k1_k1 = np.array([1]*n)
-        self.x_k_k1 = np.array([0]*n)
-        self.ADC_OLD_Value = np.array([0]*n)
-        self.kalman_adc_old = np.array([0]*n)
-        self.weight_k = 0.5
-    
-    def getValue(self, ADC_value):
-        ADC_value = np.array(ADC_value)
-        Z_k = ADC_value
-
-        if (abs(max(self.kalman_adc_old-ADC_value))>=30):
-            x_k1_k1= ADC_value*(1-self.weight_k) + self.kalman_adc_old*self.weight_k
-        else:
-            x_k1_k1 = self.kalman_adc_old
-
-        self.x_k_k1 = x_k1_k1
-        self.P_k_k1 = self.P_k1_k1 + self.Q
-
-        self.Kg = self.P_k_k1/(self.P_k_k1 + self.R)
-
-        kalman_adc = self.x_k_k1 + self.Kg * (Z_k - self.kalman_adc_old)
-        self.P_k1_k1 = (1 - self.Kg)*self.P_k_k1
-        self.P_k_k1 = self.P_k1_k1
-
-        self.ADC_OLD_Value = ADC_value
-        self.kalman_adc_old = kalman_adc
-        return kalman_adc.tolist()
-
 
 class KalmanFilterComplex():
     def __init__(self):
@@ -46,7 +13,8 @@ class KalmanFilterComplex():
         H1 = np.eye(3)
         O = np.zeros((3,12))
         H = []
-        covar = [1, 1, 1]
+        covar = [0.1, 0.1, 0.1] # диагональные элементы ковариационной матрицы измерений
+        q_diag = [0.01, 0.01, 0.01, 0.01, 0.01, 0.01]# диагональные элементы ковариационной матрицы шумов
         R = np.eye(3)
 
         for i in range(H1.shape[0]):
@@ -57,18 +25,21 @@ class KalmanFilterComplex():
 
         self.H = np.array(H)
         self.R = np.array(R)
-        self.Q = np.eye(6)*0.01
+        self.Q = np.eye(6)
+        for i in range(self.Q.shape[0]):
+            self.Q[i][i] = q_diag[i]
+
 
         self.a = 6378245
         self.b = 6356863
         self.e_2 = 0.0066934216
         self.u = 7.2922115*10**(-5)
 
-        self.xErr = [0]*15
+        self.xErr = [0]*15 # вектор состояния модели 1х15 
         self.P = np.eye(15)
 
         self.dt = 0.01
-        self.f = 55.773037
+        self.f = 55.773037 # широта начала движения
 
     def get_r1(self, f):
         return self.a*(1-self.e_2)*(1-self.e_2*sin(f)**2)**(-3/2)
@@ -175,53 +146,19 @@ class KalmanFilterComplex():
         self.xErr = xErr
         self.P = P
 
-# k_f = KalmanFilterComplex()
+# k_f = KalmanFilterComplex() # экхземпляр класса фильтра
 
-# num = 1000
-# noize = []
+# num = 1000 # количество измерений
+# noize = [] 
 # filt = []
 # ar = []
 
 # for i in range(num):
-#     noize.append(np.random.normal(i/100, 0.5))
-#     k_f.kalmanUpdate([noize[i], 0, 0], [0,0,0])
-#     filt.append(k_f.xErr[3])
+#     noize.append(np.random.normal(0, 0.5)) # генерация шума по нормальному распределению с мат ожижанием и отклонением
+#     k_f.kalmanUpdate([noize[i], 0, 0], [0,0,0]) # обновление фильтра калманаю Подается вектор разности измерений 3х1 и вектор углов ориентации [roll, pitch, yaw]
+#     filt.append(k_f.xErr[0]) # получение необходимого значени из вектора состоянияя
 #     ar.append(i+1)
 #     time.sleep(0.01)
 
 # plt.plot(ar, filt)
-# # plt.plot(ar, noize)
-# plt.show()
-
-# # print(get_dynamic_matrix(2,2,2,2,0,0,0,0))
-# # print(G(0,0,0))
-
-# array1 = np.array([100]*200)
-
-# s1 = np.random.normal(0, 5, 200)
-
-# test_array1 = array1 + s1
-# plt.plot(test_array1)
-# adc1=[]
-
-# array2 = np.array([200]*200)
-
-# s2 = np.random.normal(0, 5, 200)
-
-# test_array2 = array2 + s2
-# plt.plot(test_array2)
-# adc2=[]
-
-# kalman = KalmanFilter(2)
-
-# for i in range(200):
-#     filtred = kalman.getValue(np.array([test_array1[i], test_array2[i]]))
-#     adc1.append(filtred[0])
-#     adc2.append(filtred[1])
-
-# plt.plot(adc1)   
-# plt.plot(array1)
-
-# plt.plot(adc2)   
-# plt.plot(array2)   
 # plt.show()
